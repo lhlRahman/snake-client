7254d8ee31083542ad1490a505175f59d23b2511
Abdul Rahman <lhlrahman@gmail.com>
2023-07-15 17:45:40 -0400

    Added Names and Message for ideling.

---
+++ client.js
@@ -0,0 +1,25 @@
+// establishes a connection with the game server
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: "50541"
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data" , ()=>{
+    console.log("you ded cuz you idled");
+  });
+
+  conn.on("connect", () => {
+    console.log("Successful Connection!");
+    conn.write("Name: HAB");
+  });
+
+  return conn;
+};
+
+module.exports = connect;
