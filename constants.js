4d21280ddf29107978bd09e72478bc142ad4c5a8
Abdul Rahman <lhlrahman@gmail.com>
2023-07-15 19:51:36 -0400

    Added constants for movements and IP and PORT

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = "localhost";
+const PORT = "50541";
+const UP = "w";
+const DOWN = "s";
+const LEFT = "a";
+const RIGHT = "d";
+
+module.exports = {
+  IP,
+  PORT,
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT
+};
